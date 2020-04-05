e0c8843d4f9a5120619bcc708e1e65788ec76d83
Glitch (hello-sqlite) <none>
2020-04-05 03:50:59 +0000

    🌗🍡 Checkpoint ./bot.js:1076878/961

--- bot.js
+++ bot.js
@@ -1,11 +1,22 @@
 const Discord = require("discord.js")
 const {RichEmbed} = require("discord.js")
 const client = new Discord.Client();
+const fs = require("fs")
 
-client.on("ready", () => {
-  console.log("Started")
-});
-
-client.on("message", (msg) => {
-  
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    // If the file is not a JS file, ignore it (thanks, Apple)
+    if (!file.endsWith(".js")) return;
+    // Load the event file itself
+    const event = require(`./events/${file}`);
+    // Get just the event name from the file name
+    let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    // without going into too many details, this means each event will be called with the client argument,
+    // followed by its "normal" arguments, like message, member, etc etc.
+    // This line is awesome by the way. Just sayin'.
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
 });
