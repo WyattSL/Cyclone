9fbdbc039651bf23df9448c3cbea8329fbb45d92
Glitch (hello-sqlite) <none>
2020-04-05 06:47:41 +0000

    🍤🎾 Checkpoint ./events/message.js:1076878/267

--- events/message.js
+++ events/message.js
@@ -6,7 +6,9 @@ module.exports = (client, args) => {
       `The prefix on ${msg.guild.name} is ${client.config[msg.guild.id].prefix}`
     );
     return true;
+  } else if (!msg.content.startsWith(client.config[msg.guild.id].prefix)) {
+    return false;
   }
-  var target = msg.content.split
+  var target = msg.content.slice(client.config[msg.guild.id].prefix.length).split(" ")[0]
   var module = `/app/commands/${target}.js`
 };
