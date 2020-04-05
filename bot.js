bb2c95532f20392f15701e02de5496da7d52517e
Glitch (hello-sqlite) <none>
2020-04-05 03:39:44 +0000

    🏎🐄 Checkpoint ./README.md:1076878/957 ./package.json:1076878/7426 ./bot.js:1076878/379

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const {RichEmbed} = require("discord.js")
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("Started")
+});
+
+client.on("message", (msg) => {
+  
+});
