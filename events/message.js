b5c8baf7472ecb153c8d2ff5e1f88827f3eb3c67
Glitch (hello-sqlite) <none>
2020-04-05 06:36:25 +0000

    🏋👦 Checkpoint ./events/message.js:1076878/932 ./events/ready.js:1076878/101

--- events/message.js
+++ events/message.js
@@ -1,7 +1,12 @@
 module.exports = (client, args) => {
-  var msg = args[0]
+  var msg = args[0];
   if (msg.author.bot) return false;
   if (msg.mentions.users.first() && msg.mentions.users.first().id == client.user.id) {
-    msg.channel.send(`They `)
+    msg.channel.send(
+      `The prefix on ${msg.guild.name} is ${client.config[msg.guild.id].prefix}`
+    );
+    return true;
   }
+  var target = msg.content.split
+  var module = `/app/commands/${target}.js`
 };
--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,11 @@
 exports.run = function(client, args) {
   console.log("starting")
-  // various utilites
   client.random = function(max) {
     if (!max) {
       max = 1
     }
     Math.floor(Math.random * 10)
   }
-  
-  
-  // presence data
   var presences = require("/app/stuff/presence.json");
   client.prescount = 0
   var mod = 0
@@ -19,8 +15,6 @@ exports.run = function(client, args) {
     var p = presences[client.prescount];
     p=p.replace(/%users%/, client.users.array().length);
     p=p.replace(/%guilds%/, client.guilds.array().length);
-    console.log(p)
-    console.log(client.prescount);
     var s = p.split(" ")[0];
     p = p.slice(s.length+1);
     client.user.setPresence({status: "online", game:{ name: p, type:s}});
