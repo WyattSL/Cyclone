74c2c60dd606b03f4bb93f0b56661d90739e6bec
Glitch (hello-sqlite) <none>
2020-04-06 05:05:38 +0000

    🏡🚿 Checkpoint ./events/message.js:1076878/2 ./commands/history.js:1076878/2 ./events/ready.js:1076878/257

--- events/ready.js
+++ events/ready.js
@@ -19,16 +19,16 @@ exports.run = function(client, args) {
   client.prescount = 0
   var mod = 0
   client.setInterval(function() {
-    mod = 1000
+    mod = 15000
     if (client.prescount > presences.length-1) client.prescount = 0
     var p = presences[client.prescount];
-    p=p.replace(/%users%/, client.users.array().length);
-    p=p.replace(/%guilds%/, client.guilds.array().length);
+    p=p.replace(/%users%/, client.users.size);
+    p=p.replace(/%guilds%/, client.guilds.size);
     var s = p.split(" ")[0];
     p = p.slice(s.length+1);
     client.user.setPresence({status: "online", game:{ name: p, type:s}});
     client.prescount=client.prescount+1
-  }, (60/presences.length)*mod);
+  }, mod);
   
   // SQL
   const fs = require("fs")
