7c8c6ac2a0f6d88b67645f30563cbe60164e6954
Glitch (hello-sqlite) <none>
2020-04-16 20:23:17 +0000

    🏁🍏 Checkpoint ./commands/config.js:1076878/744 ./commands/announce.js:1076878/1167

---
+++ commands/announce.js
@@ -0,0 +1,23 @@
+exports.run = function(client, msg, args) {
+  var announcement = msg.content.slice(10, msg.content.length);
+  var channel = client.config[msg.guild.id].announceChannel;
+  if (!channel) {
+    var embed = new client.embed;
+    embed.setTitle("Oh noes!");
+    embed.setDescription("A announcement channel is not set. Please do so in the config.");
+    embed.set
+  }
+  var name = msg.guild.name;
+  var av = msg.guild.iconURL;
+  var reason = `Announcement`;
+  channel.createWebhook(name, av, reason).then(webhook => {
+    webhook.send(announcement).then(ms => {
+      webhook.delete();
+    });
+  });
+}
+
+exports.example = "announce This is a very important announcement!";
+exports.description = "Send a announcement to the channel specified in the configuration.";
+exports.usage = "announce <announcement>";
+exports.permission = "MANAGE_MESSAGES"
