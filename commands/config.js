3bf1d0110fbbd23160d857d5dc32120db0b1b0db
Glitch (hello-sqlite) <none>
2020-04-16 21:19:42 +0000

    🐆📖 Checkpoint ./commands/config.js:1076878/763

--- commands/config.js
+++ commands/config.js
@@ -1,6 +1,23 @@
 exports.run = function(client, msg, args) {
   var configlist = client.configlist;
-  
+  var key = args.shift();
+  var value = args.join(" ");
+  if (!key) {
+    var i;
+    var embed = new client.embed;
+    embed.setFooter(client.generateFooter());
+    embed.setColor(0x000000);
+    if (client.random(1) == 1) {
+      client.setTitle(`Cyclone Configuration`);
+    } else {
+      client.setTitle(`Top Secret Control Panel`);
+    }
+    for (i=0;i<configlist.length;i++) {
+      var name = configlist[i].name;
+      var description = configlist[i].description;
+      var default = configlist[i].default;
+    };
+  }
 }
 
 exports.usage = "config [key] [value] \n config [key]";
