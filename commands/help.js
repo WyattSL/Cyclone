a9a896387d9cd6a074a0f80d4e779bac36e302ab
Glitch (hello-sqlite) <none>
2020-04-17 05:24:37 +0000

    🏘🚎 Checkpoint ./commands/help.js:1076878/325

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,19 @@
+const fs = require("fs");
 exports.run = function(client, msg, args) {
   var embed = new client.embed;
   embed.setTitle("Cyclone Datacenter");
   embed.setFooter(client.generateFooter());
   var path = `/app/commands/`;
-  var i;
+  fs.readdir(path, function(err, files) {
+    if (err) throw err;
+    var i;
+    for (i=0;i<files.length;i++) {
+      var f = files[i];
+      var n = f.split(".")[0];
+      f = require(path+f);
+      console.log(f);
+    }
+  });
 }
 
 exports.usage = "help [command]";
