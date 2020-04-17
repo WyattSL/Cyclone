ee32b926207295228aac9f001a1c68f56a4c641f
Glitch (hello-sqlite) <none>
2020-04-17 09:56:38 +0000

    🛰⏲ Checkpoint ./commands/hypixel.js:1076878/1499

---
+++ commands/hypixel.js
@@ -0,0 +1,7 @@
+exports.run = function(client, msg, args) {
+  
+}
+
+exports.usage = "hypixel <username>";
+exports.description = "Retrieve hypixel stats on a player.";
+exports.example = "hypixel WyattPlayzPC"
