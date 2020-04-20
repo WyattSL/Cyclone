a76d452dc3be29208685c1d4809a9c4626be9bb6
Glitch (hello-sqlite) <none>
2020-04-20 21:00:48 +0000

    🐘🎥 Checkpoint ./events/error.js:1076878/48

--- events/error.js
+++ events/error.js
@@ -1,2 +1,3 @@
 const {Discord, RichEmbed} = require("discord.js");
-const Webhook = new Discord.Webhook(process.env.WC, process.env.WS)
+const Discord = require("discord.js");
+const Webhook = new Discord.Webhook(process.env.WC, process.env.WS);
