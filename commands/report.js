2741bc54640f161d8f6ecef18dac4c7f8e8d2608
Glitch (hello-sqlite) <none>
2020-04-20 21:35:52 +0000

    🛥😺 Checkpoint ./commands/report.js:1076878/1721 ./stuff/config.json:1076878/88

--- commands/report.js
+++ commands/report.js
@@ -1,18 +1,47 @@
 exports.run = function(client, msg, args) {
   var user = msg.mentions.members.first();
   var target;
+  var inp = args.shift();
   if (!user) {
-    var inp = args.shift();
     user = msg.guild.members.find(m => m.displayName.includes(inp));
     if (!user) {
       user = msg.guild.members.find(m => m.user.username.includes(inp));
       if (!user) {
         msg.channel.send(`I failed to find a user matching ${inp}.`);
+        return;
+      } else {
+        target = user;
       }
+    } else {
+      target = user;
     }
   } else {
     target = user;
   }
+  var ch = msg.guild.channels.find(ch => ch.id.includes(client.config[msg.guild.id].reportsChannel));
+  if (!ch) {
+    var q = "```"; // because I hate doing the bunches of breaks (\)
+    msg.channel.send(`A reports channel has not yet been defined. Please use ${q}config reportChannel #channel${q}`)
+    return false;
+  } else {
+    msg.channel.send("alrighty").then(m => {
+      var reason = args.join(" ");
+      var e = new client.embed;
+      e.setTitle("Click here to visit the message.");
+      e.setDescription(`${target.user.username}#${target.user.discriminator} was reported for ${reason}.`);
+      e.setThumbnail(target.user.displayAvatarURL);
+      e.setFooter(client.generateFooter());
+      e.setTimestamp();
+      e.setURL(m.url);
+      ch.send(e).then(ms => {
+        ms.react("no_entry");
+        ms.react("boot")
+        ms.react("mute");
+        ms.react("warning");
+        ms.react("negative_squared_cross_mark");
+      });
+    });
+  };
 };
 
 exports.usage = "report <user> <reason>";
