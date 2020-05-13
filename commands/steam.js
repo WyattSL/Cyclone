46590211e63a0696f8967720b45039dea8639c6f
Glitch (hello-sqlite) <none>
2020-05-13 22:50:20 +0000

    🚜🌸 Checkpoint ./commands/steam.js:1076878/1116

--- commands/steam.js
+++ commands/steam.js
@@ -1,7 +1,29 @@
-exports.run = function(client, msg, args) {
-  var url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_KEY}&steamids=${id}`
+const got = require("got");
+
+exports.run = async function(client, msg, args) {
+  var url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${process.env.STEAM_API}&vanityurl=${args.join(" ")}`;
+  var id;
+  var req = await got(url);
+  console.log(req.body);
+  var d = JSON.parse(req.body).response;
+  var e = d.success
+  if (e == 1) {
+    id=d.steamid;
+    var url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API}&steamids=${id}`;
+    var req = await got(url);
+    console.log(req.body);
+    var d = JSON.parse(req.body).response
+    var e = new client.embed;
+    e.setTitle("Lookup Results");
+    e.setAuthor()
+    msg.channel.send(e);
+  } else if (e == 42) {
+    msg.channel.send(`I was unable to find a user with that vanity url.`);
+  } else {
+    msg.channel.send(`API Error: [${e}] ${d.message}`);
+  }
 }
 
-exports.usage = "steam <username/pageurl>";
+exports.usage = "steam <vanityurl>";
 exports.description = "Retreive information on a steam user.";
 exports.example = "steam WyattL"
