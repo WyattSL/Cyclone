a967783eb8c755340b2c1c86c0b3b1384c8881e6
Glitch (hello-sqlite) <none>
2020-05-13 23:02:19 +0000

    🍂💁 Checkpoint ./commands/steam.js:1076878/435

--- commands/steam.js
+++ commands/steam.js
@@ -12,10 +12,17 @@ exports.run = async function(client, msg, args) {
     var url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API}&steamids=${id}`;
     var req = await got(url);
     console.log(req.body);
-    var d = JSON.parse(req.body).response
+    var d = JSON.parse(req.body).response.players[0];
     var e = new client.embed;
     e.setTitle("Lookup Results");
-    e.setAuthor()
+    e.setAuthor(d.personaname, d.avatar, d.profileurl)
+    e.setThumbnail(d.avatarfull);
+    e.addField("Steam ID", d.steamid);
+    if (!d.profilestate) {
+      e.setDescription("This user has not setup their profile.");
+    } else if (d.communityvisiblitystate < 3) {
+      e.setDescription("This user's profile is not public.");
+    }
     msg.channel.send(e);
   } else if (e == 42) {
     msg.channel.send(`I was unable to find a user with that vanity url.`);
