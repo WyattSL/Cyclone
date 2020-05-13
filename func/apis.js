40ae45d3eabf86f8f2f6380fc2dfcd5414ded674
Glitch (hello-sqlite) <none>
2020-05-13 21:44:13 +0000

    🐢🍔 Checkpoint ./func/apis.js:1076878/260

--- func/apis.js
+++ func/apis.js
@@ -1,11 +1,14 @@
 const got = require("got");
+const amplify = 20;
 
 exports.run = function(client) {
-  got.post("discord.botsondiscord.xyz/bots/", {
+  got.post("https://discord.botsondiscord.xyz/bot-api/bots/", + client.user.id + "/guilds", {
       "headers": {
       "Authorization": process.env.BOD_API,
       "Content-Type": "application/json"
     },
     "body": JSON.stringify({"guildCount": client.guilds.size+amplify*2})
   });
+  // -----------------------------------------------------------------------------
+  got.post("https://discord.boats/")
 }
