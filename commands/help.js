8a1332c498bbb51554ea418df25782dabc94f31e
Glitch (hello-sqlite) <none>
2020-05-16 01:44:39 +0000

    🏠👰 Checkpoint ./package.json:1076878/5382 ./commands/help.js:1076878/767

--- commands/help.js
+++ commands/help.js
@@ -1,32 +1,43 @@
 const fs = require("fs");
 exports.run = function(client, msg, args) {
-  var embed = new client.embed;
-  embed.setTitle("Cyclone Datacenter");
-  embed.setDescription("You will only see commands you have access to.")
-  embed.setFooter(client.generateFooter());
-  var path = `/app/commands/`;
-  fs.readdir(path, function(err, files) {
-    if (err) throw err;
-    var i;
-    for (i=0;i<files.length;i++) {
-      var f = files[i];
-      var n = f.split(".")[0];
-      f = require(path+f);
-      var prefix = client.config[msg.guild.id].prefix || ".";
-      var perm = f.permission;
-      if (perm) perm=perm.replace("MANAGE_SERVER", "MANAGE_GUILD");
-      if (!f.hide) {
-        if (!f.meonly || msg.author.id == process.env.OWNER) {
-          if (!f.permission) {
-            embed.addField(prefix + f.usage, f.description, false);
-          } else if (msg.member.hasPermission(perm, false, true, true)) {
-            embed.addField(prefix + f.usage, f.description, false);
+  if (!args[0]) {
+    var embed = new client.embed;
+    embed.setTitle("Cyclone Datacenter");
+    embed.setDescription("You will only see commands you have access to.")
+    embed.setFooter(client.generateFooter());
+    var path = `/app/commands/`;
+    fs.readdir(path, function(err, files) {
+      if (err) throw err;
+      var i;
+      for (i=0;i<files.length;i++) {
+        var f = files[i];
+        var n = f.split(".")[0];
+        f = require(path+f);
+        var prefix = client.config[msg.guild.id].prefix || ".";
+        var perm = f.permission;
+        if (perm) perm=perm.replace("MANAGE_SERVER", "MANAGE_GUILD");
+        if (!f.hide) {
+          if (!f.meonly || msg.author.id == process.env.OWNER) {
+            if (!f.permission) {
+              embed.addField(prefix + f.usage, f.description, false);
+            } else if (msg.member.hasPermission(perm, false, true, true)) {
+              embed.addField(prefix + f.usage, f.description, false);
+            }
           }
         }
       }
+      msg.channel.send(embed);
+    });
+  } else {
+    var embed = new client.embed;
+    embed.setTitle("Cyclone Datacenter Results");
+    var file = `./${args[0]}.js`;
+    if (fs.existsSync(file)) {
+      
+    } else {
+      embed.setDescription("")
     }
-    msg.channel.send(embed);
-  });
+  }
 }
 
 exports.usage = "help [command]";
