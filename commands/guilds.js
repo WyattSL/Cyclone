02aefa6bf2d3f996dd01c0850680ea010287e1d7
Glitch (hello-sqlite) <none>
2020-05-16 02:20:38 +0000

    🛫🎪 Checkpoint ./stuff/config.json:1076878/153 ./commands/guilds.js:1076878/73

--- commands/guilds.js
+++ commands/guilds.js
@@ -5,3 +5,4 @@ exports.run = function(client, msg, args) {
 exports.usage = "guilds"
 exports.example = "guilds"
 exports.description = "List all guilds the bot is in. Use the config to disable this."
+exports.p = ["SEND_MESSAGES", "EMBED_LINKS"]
