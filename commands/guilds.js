627292e16e4da59558624d41123fcb2d2798de35
Glitch (hello-sqlite) <none>
2020-05-16 02:32:38 +0000

    🧀👮 Checkpoint ./commands/guilds.js:1076878/734

--- commands/guilds.js
+++ commands/guilds.js
@@ -1,5 +1,17 @@
 exports.run = function(client, msg, args) {
-  
+  var embed = new client.embed;
+  embed.setTitle("Guild List");
+  embed.setColor(0x0000FF);
+  embed.setFooter(client.generateFooter())
+  embed.setTimestamp();
+  var guilds = client.guilds.array();
+  var desc = "";
+  var i;
+  for (i=0;i<guilds.length;i++) {
+    var guild = guilds[i];
+    if (!client.config[guild.id]["listGuilds"] || client.config[guild.id]["listGuilds"] == "false") return false;
+    if (client.config[guild.id])
+  }
 }
 
 exports.usage = "guilds"
