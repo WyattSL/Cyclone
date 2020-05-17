44dfc397939f78fa3241b1357a1d140979ec0e85
Glitch (hello-sqlite) <none>
2020-05-17 06:29:51 +0000

    🏈🌸 Checkpoint ./commands/dashboard.js:1076878/414

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -1,7 +1,14 @@
+const crypto = require("crypto");
 exports.run = function(client, msg, args) {
   var q = `DELETE FROM weblinks WHERE guild=?`;
   client.db.run(q, msg.guild.id);
-  var randomId
+  var secret = process.env.SECRET
+  var hash = crypto.createHmac('sha256', secret)
+   .update('I love cupcakes')
+   .digest('hex');
+  console.log(hash);
+  var q = `INSERT INTO weblinks ("guild", "hash") VALUES (@0, @1)`;
+  client.db.run(q, msg.guild.id, hash);
 };
 
 exports.owneronly = true;
