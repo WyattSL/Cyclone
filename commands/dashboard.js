ac99ec5aedb5cce66f933302f1911bbe82b62b82
Glitch (hello-sqlite) <none>
2020-05-17 06:05:51 +0000

    🍏🌺 Checkpoint ./commands/dashboard.js:1076878/325

---
+++ commands/dashboard.js
@@ -0,0 +1,8 @@
+exports.run = function(client, msg, args) {
+  
+};
+
+exports.owneronly = true;
+exports.description = "Receive a link to login to the website dashboard.";
+exports.usage = "dashboard";
+exports.example = "dashboard";
