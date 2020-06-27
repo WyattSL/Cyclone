811f2f38162cd9673dd2173845e1849c9ee25144
Glitch (wl-cyclone) <none>
2020-06-27 01:49:55 +0000

    ⛩🎮 Checkpoint ./commands/stats.js:1076878/515

--- commands/stats.js
+++ commands/stats.js
@@ -5,9 +5,16 @@ exports.run = function(client, msg, args) {
     e.setAuthor(msg.guild.owner.user.tag, msg.guild.owner.user.displayAvatarURL)
     var channels = [
         `Total Channels: ${msg.guild.channels.size}`,
-        ``
+        `Text Channels: ${msg.guild.channels.filter(ch => ch.type == "text").size}`,
+        `Voice Channels: ${msg.guild.channels.filter(ch => ch.type == "voice").size}`,
+        `Categories: ${msg.guild.channel.filter(ch => ch.type == "category").size}`
     ]
-    e.addField(channels.concat("\n"))
+    e.addField("Channels", channels.concat("\n"))
+    var roles = [
+        `Total: ${msg.guild.roles.size}`,
+        `--- List ---`,
+        `${msg.guild.roles.concat("\n")}`
+    ]
 }
   
 exports.usage = "stats"
