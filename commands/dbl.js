894af0ab76df6dcc1becf4c3f607f7bd0f048c55
Glitch (wl-cyclone) <none>
2020-06-27 18:05:58 +0000

    💡🏊🏼 Checkpoint ./commands/dbl.js:1076878/2906

--- commands/dbl.js
+++ commands/dbl.js
@@ -1,16 +1,38 @@
-exports.run = function(client, msg, args) {
+exports.run = async function(client, msg, args) {
     const dbl = client.dbl;
     var type = args.shift();
     if (!type) {
         return 1;
     }
-    if (type == "bot") {
-        
-    } else if (type == "user") {
+    msg.channel.send(`Please wait...`).then(async ms => {
+        if (type == "bot") {
+            var query = args.join(" ");
+            var bot;
+            if (!Number(query)) {
+                var bots = await dbl.getBots({limit: 1, search: "username: " + query})
+                console.log(bots);
+                if (bots.results && bots.results[0]) {
+                    bot = bots.results[0];
+                } else {
+                    ms.delete();
+                    return 2;
+                }
+            } else {
+                bot = dbl.getBot(query)
+            }
+            console.log(bot);
+            var embed = new client.embed;
+            embed.setTitle("Discord Bot List");
+            embed.setURL("https://top.gg/bot/" + bot.id);
+            embed.setAuthor(bot.username, bot.avatar)
+            ms.edit(embed);
+        } else if (type == "user") {
 
-    } else {
-        return 1;
-    }
+        } else {
+            ms.delete();
+            return 1;
+        }
+    });
 };
 
 exports.usage = "dbl <bot/user> <name/id>"
