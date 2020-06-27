0f8f810e90d9b1db1a1b477384c586f274427126
Glitch (wl-cyclone) <none>
2020-06-27 19:04:16 +0000

    ☃️🍕 Checkpoint ./commands/roblox.js:1076878/391

--- commands/roblox.js
+++ commands/roblox.js
@@ -1,5 +1,7 @@
+const got = require('got');
+
 exports.run = function(client, msg, args) {
-    msg.channel.send("Please wait...").then(ms => {
+    msg.channel.send("Please wait...").then(async ms => {
         var user = args.join(" ");
         var url = `https://www.roblox.com/search/users/results?keyword=${user}&maxRows=1&startIndex=0`
         var req = await got(url);
@@ -8,14 +10,18 @@ exports.run = function(client, msg, args) {
             return 2;
         }
         var id = res[0].UserId
-        var embed = new RichEmbed;
+        var embed = new client.embed;
         embed.setTitle("Roblox Data Retrieval")
         embed.setFooter(client.generateFooter())
         embed.setColor(0x000000)
         var url = `https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${id}&size=150x150&format=png`
         var areq = await got(url);
-        var ares = JSON.parse(areq).data[0]
-        embed.setAuthor(res.DisplayName, ares.imageUrl)
+        var ares = JSON.parse(areq.body).data[0]
+        embed.setAuthor(res[0].DisplayName, ares.imageUrl)
+        var url = `https://thumbnails.roblox.com/v1/users/avatar?userIds=${id}&size=352x352&format=png`;
+        var breq = await got(url);
+        var bres = JSON.parse(breq.body).data[0];
+        embed.setThumbnail(bres.imageUrl);
         embed.setURL(`https://roblox.com/users/${id}/profile`);
         var url = `https://www.roblox.com/search/users/presence?userIds=${id}`;
         var req = await got(url);
