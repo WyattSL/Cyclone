d07ab5d839aea298e5ba272afb2f1bb3da91789c
Glitch (wl-cyclone) <none>
2020-06-27 03:21:28 +0000

    🐕👔 Checkpoint ./commands/stats.js:1076878/80

--- commands/stats.js
+++ commands/stats.js
@@ -4,26 +4,26 @@ exports.run = async function(client, msg, args) {
         var e = new client.embed;
         e.setTitle(guild.name)
         e.setThumbnail(msg.iconURL)
-        e.setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL)
+        if (guild.owner) e.setAuthor(guild.owner.user.tag, guild.owner.user.displayAvatarURL)
         var channels = [
             `Total Channels: ${guild.channels.size}`,
             `Text Channels: ${guild.channels.filter(ch => ch.type == "text").size}`,
             `Voice Channels: ${guild.channels.filter(ch => ch.type == "voice").size}`,
-            `Categories: ${guild.channel.filter(ch => ch.type == "category").size}`
+            `Categories: ${guild.channels.filter(ch => ch.type == "category").size}`
         ]
         e.addField("Channels", channels.concat("\n"))
         var roles = [
             `Total: ${guild.roles.size}`,
             `--- List ---`,
-            `${guild.roles.concat("\n")}`
+            `${guild.roles.array().concat("\n")}`
         ]
         e.addField("Roles", roles.concat("\n"));
         if (guild.description) e.addField("Description", guild.description);
-        if (guild.features) e.addField("Features", guild.features.concat(", "));
-        if (guild.premiumSubscriptions) e.addField(`Tier ${guild.premiumTier}`, `:booster: ${guild.premiumSubscriptions}`);
+        if (guild.features) e.addField("Features", guild.features.array().concat(", "));
+        if (guild.premiumSubscriptionCount) e.addField(`Tier ${guild.premiumTier}`, `:booster: ${guild.premiumSubscriptionCount}`);
         if (guild.region) e.addField(`Region`, guild.region)
         if (guild.verified) e.addField(`Verified`, `:heavy_check_mark:`)
-        e.setFooter(`${client.generateFooter} | Guild created at`);
+        e.setFooter(`${client.generateFooter()} | Guild created on`);
         e.setTimestamp(guild.createdTimestamp)
         msg.channel.send(e);
     });
